2101a8f8ce98fe17149d3b8aaceb32482e9d64ca
Victor Pereira <Lnvictor@github.com>
2020-10-11 14:12:20 -0300

    postgres

--- todosite/settings.py
+++ todosite/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from functools import partial
+
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,19 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "_33f6&!0#v&ca4kalcdazxah*r%qoqewzd*3v*m#!gnuu2+=z7"
-SECRET_KEY = os.environ.get("SECRET_KEY")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = [
-    "0.0.0.0",
-    "localhost",
-    "127.0.0.1",
-    "to-dosite.herokuapp.com",
-]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -86,11 +82,13 @@ WSGI_APPLICATION = "todosite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+default_db_url = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": config(
+        "DATABASE_URL", default=default_db_url, cast=parse_database
+    )
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
